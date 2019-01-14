0edd7e680d937584c8ee4acfda552fad6c993447
André Baroni <andrelbaroni1@gmail.com>
2019-01-14 09:46:26 -0200

    calcula vazao

---
+++ lista 2/ex3.js
@@ -0,0 +1,14 @@
+//ex3 
+
+function bomba(vazao, cap){
+    //vazao litros por segundo
+    //cap em litros
+    var s = cap/vazao // segundos
+    var m = s / 60 //minutos
+    var h = m / 60 // horas
+    console.log("Segundos: "+s)
+    console.log("Minutos: " +m)
+    console.log("Horas: "+h)
+}
+
+console.log(bomba(4, 2000))
