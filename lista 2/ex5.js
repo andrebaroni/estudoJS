2d8cb70dc42ef234a2f92fe699698927f7be241a
André Baroni <andrelbaroni1@gmail.com>
2019-01-14 09:47:11 -0200

    calcula preco com margem de lucro

---
+++ lista 2/ex5.js
@@ -0,0 +1,16 @@
+function lucro(preco){
+    preco = preco + (preco*0.45) //14,5
+    preco = preco - (preco*0.2) // 11,6
+    return preco
+}
+
+
+function test(preco){
+    console.log("Preco de fabrica: " + preco)
+    preco = preco + (preco*0.15) //preco com lucro
+    preco = preco + (preco*0.25) //preco com margem de desconto
+    console.log("Preco com 15% de margem de lucro: " + (preco-preco*0.2))
+    return preco
+}
+
+console.log("Preço Final: " + test(100))
